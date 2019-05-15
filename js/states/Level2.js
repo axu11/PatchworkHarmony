3f79cb4eff54faafd44f7cf101754e2a12d86eee
Andrew <axu11@ucsc.edu>
2019-05-15 11:12:48 -0700

    Create Level2.js

---
+++ js/states/Level2.js
@@ -0,0 +1,16 @@
+// Second level, rooftops
+var Level2 = function(game) {};
+Level2.prototype = {
+	init: function() {
+		numPlatforms = 0;
+	},
+	create: function() {
+
+	},
+	update: function() {
+
+	},
+	render: function() {
+
+	}
+}
