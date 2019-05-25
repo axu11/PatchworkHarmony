7b725883945ab6bf20f64e769b7c98beac9e9fb4
Anfernee Lai <anferneelai1129@gmail.com>
2019-05-25 16:47:23 -0700

    dropped platform

--- js/states/MainMenu.js
+++ js/states/MainMenu.js
@@ -17,7 +17,7 @@ MainMenu.prototype = {
 	update: function() {
 		// Go to Play state once SPACEBAR pressed
 		if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
-			game.state.start('Level2');
+			game.state.start('Play');
 		}
 	}
 }
